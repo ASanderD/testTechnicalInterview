501093d74f543110a86230eed55a8caf3ad7fa34
ASanderD <142533947+ASanderD@users.noreply.github.com>
2024-06-23 19:27:41 +0700

    Задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+/*
+Билет № 4
+Задача
+Напишите код, с помощью которого можно разделить строку на части
+*/
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "У Лукоморья дуб зеленый!";
+        String[] split = str1.split(" ", 3);
+        System.out.println(Arrays.toString(split));
+        String str2 = str1.substring(str1.length() / 2);
+        System.out.println(str2);
+
     }
 }
